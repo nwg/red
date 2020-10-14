2f1c7973f632f7759e4b0ae329736326fcee594a
Nathaniel Griswold <nategriswold@gmail.com>
2020-10-14 09:03:58 -0500

    load file on backend again

--- src/libred/src/libred.c
+++ src/libred/src/libred.c
@@ -24,40 +24,6 @@ void file_changed_callback(const char *path, void *data) {
 }
 
 int libred_load_file(const char *fn) {
-  __block bool changed = false;
-  fs_start_watching_file(fn, ^{
-      printf("File changed block\n");
-      changed = true;
-    });
-
-  int fd = open(fn, O_RDONLY);
-  if (fd < 0) return -1;
-
-  struct stat s;
-  int status = fstat(fd, &s);
-  if (status < 0) return -1;
-
-  size_t len = s.st_size;
-
-  const char *mapped = mmap(0, len, PROT_READ, MAP_PRIVATE, fd, 0);
-  if (mapped == MAP_FAILED) return -1;
-
-  
-  while (!changed) {
-    // read file
-    sleep(5);
-    break;
-  }
-
-  fs_stop_watching_file();
-  close(fd);
-  munmap((void*)mapped, len);
-
-  if (changed) {
-    printf("Error -- file %s changed on disk during read\n", fn);
-    return -1;
-  }
-  
-  return 0;
+  return mm_client_backend_load_file(fn);
 }
 
