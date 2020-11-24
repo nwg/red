8c16f5acf4ae611fa05a0c2600eb60f41cc2ad2a
Nathaniel Griswold <nategriswold@gmail.com>
2020-11-24 03:53:17 -0600

    remove comments

--- src/gui/osx/Red/AppDelegate.swift
+++ src/gui/osx/Red/AppDelegate.swift
@@ -45,43 +45,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let frame = CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height)
         self.textScrollView.frame = frame
         window.contentView?.addSubview(self.textScrollView)
-
-//
-//        let path = "/tmp/test.txt"
-//        clientQueue.async {
-//            print("Running on socket \(socketFile.path)")
-//
-//
-//            var result = libred_init("ipc://".appending(socketFile.path))
-//            if result != 0 { abort() }
-//
-//            var buf : OpaquePointer?
-//            result = libred_load_file(path, &buf)
-//            if result != 0 { abort() }
-            
-//            let width = 800
-//            let height = 600
-//            let size = width * height * 4
-//
-//            var shm : OpaquePointer?
-//            result = libred_create_and_attach_shared_memory(size, &shm)
-//            if result != 0 { abort() }
-//
-//            var portal : OpaquePointer?
-//            result = libred_open_portal(shm, Int32(width), Int32(height), &portal)
-//            if result != 0 { abort() }
-//
-//            result = libred_draw_buffer_in_portal(buf, portal)
-//            if result != 0 { abort() }
-            
-
-//            DispatchQueue.main.async {
-//                let addr = libred_shm_get_addr(shm)
-//                let bytes = addr?.bindMemory(to: UInt8.self, capacity: size)
-//                let data = CFDataCreateWithBytesNoCopy(nil, bytes, size, nil)
-//                self.textScrollView!.textPortalView!.setupImage(data: data!, width: width, height: height)
-//            }
-//        }
+        
                 
         let bundle = Bundle(identifier: "org.racket-lang.Racket")!
         let petite = bundle.bundleURL.appendingPathComponent("Versions/Current/boot/petite.boot")
